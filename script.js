b0934c30e67b5586b9b94a2090f446c5a6561905
Sasikumar.V <155644823+sasikumarvcse@users.noreply.github.com>
2025-06-20 12:29:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// script.js
-
-// Initialize AOS library
-AOS.init({
-  duration: 1000,
-  once: true,
-});
-
-// Example form handler
-function handleForm(e) {
-  e.preventDefault();
-  alert("Thank you! Your message has been received.");
-  e.target.reset(); // clear form fields
-}
-
 
